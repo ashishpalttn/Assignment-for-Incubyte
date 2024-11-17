606fc70d11a384f752b724eaa78e1a34e5c4e956
Ashish <ashish.p@lecturenotes.in>
2024-11-17 17:25:30 +0530

    handking multiple common-separated numbers

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -11,3 +11,7 @@ test('should return the number when a single number is provided', () => {
 test('should return the sum of two common-separated numbers',()=>{
     expect(stringCalculator.add("1,5")).toBe(6);
 })
+
+test('should return the sum of multiple comma-separated numbers', () => {
+    expect(stringCalculator.add("10,20,30")).toBe(60);
+});
