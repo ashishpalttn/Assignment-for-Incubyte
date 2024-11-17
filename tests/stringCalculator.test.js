fc1b79784e6e805c65fc80eb1f102666ace32fd9
Ashish <ashish.p@lecturenotes.in>
2024-11-17 16:45:29 +0530

    adding new failing test case

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -3,3 +3,7 @@ const stringCalculator = require('../src/stringCalculator');
 test('should return 0 for an empty string', () => {
     expect(stringCalculator.add("")).toBe(0);
 });
+
+test('should return the number when a single number is provided', () => {
+    expect(stringCalculator.add("5")).toBe(5);
+});
